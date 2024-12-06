fb46ab42f07186deca31607327ce35bdee846d0f
BonoX23 <ricardo.bonos@gmail.com>
2024-12-06 15:32:28 -0300

    Criando Dtos

---
+++ LaiseEsteticaService/Core/Application/Application/Customer/Dtos/CustomerDto.cs
@@ -0,0 +1,28 @@
+﻿using Application.Aesthetic.Dtos;
+using Application.PathologicalHistory.Dtos;
+using Application.ProfessionalEvaluation.Dtos;
+using Application.SocialHistory.Dtos;
+using Domain.Customer.Enums;
+
+namespace Application.Customer.Dtos;
+
+public class CustomerDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public MaritalStatus MaritalStatus { get; set; }
+    public string Address { get; set; }
+    public string Neighborhood { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string Occupation { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Complaint { get; set; }
+    public SocialHistoryDto SocialHistoryDto { get; set; }
+    public PathologicalHistoryDto PathologicalHistoryDto { get; set; }
+    public AestheticHistoryDto AestheticHistoryDto { get; set; }
+    public ProfessionalEvaluationDto ProfessionalEvaluationDto { get; set; }
+}
---
+++ LaiseEsteticaService/Core/Application/Application/PathologicalHistory/Dtos/PathologicalHistoryDto.cs
@@ -0,0 +1,30 @@
+﻿using Application.Customer.Dtos;
+using Domain.Enums;
+using Domain.PathologicalHistory.Entities;
+using Domain.PathologicalHistory.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.PathologicalHistory.Dtos;
+
+public class PathologicalHistoryDto
+{
+    public Guid Id { get; set; }
+    public Surgery Surgery { get; set; }
+    public MedicalTreatment MedicalTreatment { get; set; }
+    public Medication Medication { get; set; }
+    public MedicationAllergy MedicationAllergy { get; set; }
+    public Frequency BowelFunction { get; set; }
+    public MenstrualCycle MenstrualCycle { get; set; }
+    public Contraceptives Contraceptives { get; set; }
+    public Frequency SuspectedPregnancy { get; set; }
+    public BloodPressure BloodPressure { get; set; }
+    public Endocrine EndocrineProblems { get; set; }
+    public HeartProblems HeartProblems { get; set; }
+    public OncologicalProblem? OncologicalProblemDetails { get; set; }
+    public Guid CustomerId { get; set; }
+    public CustomerDto CustomerDto { get; set; }
+}
