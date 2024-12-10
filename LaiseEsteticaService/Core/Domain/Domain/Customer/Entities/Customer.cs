ef8b7c4cb691800131bb0e96872754f430d851d2
BonoX23 <ricardo.bonos@gmail.com>
2024-12-09 23:53:30 -0300

    Criando contexto

--- LaiseEsteticaService/Core/Domain/Domain/Customer/Entities/Customer.cs
+++ LaiseEsteticaService/Core/Domain/Domain/Customer/Entities/Customer.cs
@@ -1,21 +1,11 @@
-﻿using Domain.Customer.Enums;
+﻿using Domain.Identity;
 
 namespace Domain.Customer.Entities;
 
 public class Customer
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
-    public DateTime DateOfBirth { get; set; }
-    public MaritalStatus MaritalStatus { get; set; }
-    public string Address { get; set; }
-    public string Neighborhood { get; set; }
-    public string City { get; set; }
-    public string State { get; set; }
-    public string ZipCode { get; set; }
-    public string Occupation { get; set; }
-    public string Phone { get; set; }
-    public string Email { get; set; }
+    public User User { get; set; }
     public string Complaint { get; set; }
     public SocialHistory.Entities.SocialHistory SocialHistory { get; set; }
     public PathologicalHistory.Entities.PathologicalHistory PathologicalHistory { get; set; }
---
+++ LaiseEsteticaService/Core/Domain/Domain/Identity/User.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Customer.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Identity;
+
+public class User : IdentityUser<int>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public MaritalStatus MaritalStatus { get; set; }
+    public string Address { get; set; }
+    public string Neighborhood { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string Occupation { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public IEnumerable<UserRole> UserRoles { get; set; }
+}
